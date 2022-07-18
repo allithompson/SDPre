d35c11ccbfe0b8fcf779ec662a67eb1ef04e2c3d
Alli Thompson <alli.rene.thompson@gmail.com>
2022-07-18 12:28:17 -0400

    complete objective2lab3

---
+++ Labs/Objective2Lab3.java
@@ -0,0 +1,13 @@
+public class Objective2Lab3 {
+  public static void main(String[] args) {
+    int yearYouWereBorn = 1996;
+    String monthYouWereBorn = "June";
+    boolean weekend = false;
+
+    System.out.println(yearYouWereBorn);
+    System.out.println(monthYouWereBorn);
+    System.out.println(weekend);
+
+
+  }
+}
