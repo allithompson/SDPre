c1bbe47100d0a638866fe66df316be46434e7799
Alli Thompson <alli.rene.thompson@gmail.com>
2022-08-01 15:07:08 -0400

    complete objective8lab2

---
+++ Labs/Objective8Lab2.java
@@ -0,0 +1,11 @@
+public class Objective8Lab2 {
+  public static void main(String[] args) {
+    int counter = 1;
+    int sum = 0;
+    while(counter <= 20) {
+      sum = counter + sum;
+      counter = counter + 1;
+    }
+    System.out.println(sum);
+  }
+}
