a5fb7fe46029c08cb9d71896bbb6b794079c8451
Alli Thompson <alli.rene.thompson@gmail.com>
2022-08-01 15:15:57 -0400

    complete objective8lab1

--- Labs/Objective8Lab1.java
+++ Labs/Objective8Lab1.java
@@ -1,15 +1,11 @@
 public class Objective8Lab1 {
   public static void main(String[] args) {
-   int i = 1;
-   int f = 10;
 
-   while(i <= 10) {
+   for(int i = 1; i <= 10; i++) {
      System.out.println(i);
-     i = i + 1;
    }
-while(f >= 1) {
-  System.out.println(f);
-  f = f -1;
-}
+   for(int i = 10; i >= 1; i--) {
+     System.out.println(i);
+   }
 }
 }
